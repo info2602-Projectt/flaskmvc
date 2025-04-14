ef1e1fdc374669b0316eb62ba63b47eb68475624
SamHussain101 <157634563+SamHussain101@users.noreply.github.com>
2025-04-14 16:41:07 -0400

    Update amenity.py

--- App/controllers/amenity.py
+++ App/controllers/amenity.py
@@ -11,3 +11,19 @@ def add_amenity():
     db.session.add(new_amenity)
     db.session.commit()
     return jsonify({'id': new_amenity.id, 'name': new_amenity.name}), 201
+
+def update_amenity(id, name=None):
+    amenity = get_amenity(id)
+    if amenity and name:
+        amenity.name = name
+        db.session.commit()
+        return amenity
+    return None
+
+def delete_amenity(id):
+    amenity = get_amenity(id)
+    if amenity:
+        db.session.delete(amenity)
+        db.session.commit()
+        return True
+    return False
