7cd6d480c53104eb0d5ccd3363b082639b27a625
Aidan Hosein <aidanhosein100@gmail.com>
2025-04-18 04:29:25 +0000

    fixed user for test cases

--- App/models/user.py
+++ App/models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
-    email=email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     role = db.Column(db.String(20), nullable=False)  # 'landlord' or 'tenant'
     is_verified = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
@@ -15,6 +15,11 @@ class User(db.Model):
     listings = db.relationship('Apartment', backref='owner', lazy=True)
     reviews = db.relationship('Review', backref='author', lazy=True)
 
+    def __init__(self, username, password, email, role):
+        self.username = username
+        self.email = email
+        self.role = role
+        self.set_password(password) 
 
     def get_json(self):
         return {
